9c065ce798d6d5630844883beea5e00b4799b5ec
jeffpeng3 <jeffpeng2012@gmail.com>
2022-07-21 03:48:42 +0800

    Write some shit code and go to bed pissed off

---
+++ musicplayer/Controls/HideableSlider.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Ui.Controls;
+
+namespace musicplayer.Controls
+{
+    /// <summary>
+    /// HideableSlider.xaml 的互動邏輯
+    /// </summary>
+    public partial class HideableSlider : UserControl
+    {
+        public HideableSlider()
+        {
+            InitializeComponent();
+        }
+
+        private void StartShow(object sender, MouseEventArgs e)
+        {
+        }
+    }
+}
