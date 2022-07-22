ead3c5492f6979b14947d95a3026934776edc480
CCcat <linnemo920317@gmail.com>
2022-07-22 17:09:07 +0800

    player and song basic frame そう　なん　しえ

--- musicplayer/Enums/MessageQueue.cs
+++ musicplayer/Enums/MessageQueue.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace musicplayer.Modules
+namespace musicplayer.Enums
 {
-    public enum MessageTopic
+    public enum MessageTopicEnum
     {
         QueueUpdate,
         PlayingUpdate,
--- musicplayer/Modules/Player.cs
+++ musicplayer/Modules/Player.cs
@@ -1,225 +1,89 @@
-﻿using Microsoft.VisualStudio.Threading;
-using musicplayer.Enums;
-using Nito.Collections;
+﻿using musicplayer.Enums;
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Media;
-using YoutubeExplode;
-using YoutubeExplode.Videos.Streams;
 
 namespace musicplayer.Modules
 {
-
-
-
     public class Player
     {
-        readonly MediaPlayer player = new();
-        readonly ObservableCollection<Music> queue = new();
-        public int index;
-        public bool Mute;
-        public LoopModeEnum LoopMode;
-        public double volume;
-        public Music? nowPlaying;
-        public PlayStatusEmun status;
-        readonly AsyncQueue<MessageTopic> MessageQueue;
-
-
-        public Player(AsyncQueue<MessageTopic> _MessageQueue)
+        readonly ObservableCollection<Song> SongList = new();
+        // List<int> ReapeatTimes;
+        int ListIndex;
+        double Volume
         {
-            Mute = false;
-            status = PlayStatusEmun.NotPlaying;
-            MessageQueue = _MessageQueue;
-            player.MediaEnded += AfterPlay;
-            index = 1;
+            get { return Volume; }
+            set
+            {
+
+            }
+        }
+        bool isMute
+        {
+            get { return isMute; }
+            set
+            {
+
+            }
+        }
+        TimeSpan Position
+        {
+            get { return Position; }
+            set
+            {
+
+            }
+        }
+        PlayStatusEmun Status
+        {
+            get { return Status; }
+            set
+            {
+
+            }
+        }
+        LoopModeEnum LoopMode
+        {
+            get { return LoopMode; }
+            set
+            {
+
+            }
+        }
+        public Player()
+        {
+            ListIndex = 0;
+            Volume = 0.5;
+            isMute=false;
+            Position = TimeSpan.Zero;
+            Status = PlayStatusEmun.NotPlaying;
             LoopMode = LoopModeEnum.LoopNone;
-            nowPlaying = null;
-            volume = 0.25;
-            _ = TickAsync();
         }
-
-        private async Task TickAsync()
+        public ObservableCollection<Song> GetSongs() => SongList;
+        public void Play(int index = -1)
         {
-            var Last = player.Position;
-            while (true)
-            {
-                if (Last != player.Position)
-                {
-                    MessageQueue.Enqueue(MessageTopic.TickUpdate);
-                    Last = player.Position;
-                }
-                await Task.Delay(333);
-            }
-        }
 
-        private void AfterPlay(object? sender, EventArgs e)
+        }
+        public void Resume()
         {
-            switch (LoopMode)
-            {
-                case LoopModeEnum.LoopNone:
-                    if (nowPlaying != null)
-                    {
-                        index++;
-                    }
-                    break;
-                case LoopModeEnum.LoopAll:
-                    if (nowPlaying != null)
-                    {
-                        index = index == queue.Count ? 1 : index + 1;
-                    }
-                    break;
-            }
-            nowPlaying = null;
-            status = PlayStatusEmun.NotPlaying;
-            TryToPlayMusic(false);
-            MessageQueue.Enqueue(MessageTopic.PlayingUpdate);
-            MessageQueue.Enqueue(MessageTopic.StatusUpdate);
-        }
-
-        private void TryToPlayMusic(bool IsInternal = true)
-        {
-            if (nowPlaying == null)
-            {
-                if (index <= queue.Count)
-                {
-                    var song = queue[index - 1];
-                    player.Open(song.AudioStream);
-                    player.Play();
-                    nowPlaying = song;
-                    status = PlayStatusEmun.Play;
-                    if (IsInternal)
-                    {
-                        MessageQueue.Enqueue(MessageTopic.PlayingUpdate);
-                        MessageQueue.Enqueue(MessageTopic.StatusUpdate);
-                    }
-                }
-            }
-        }
-
-        public void AddQueue(Music music)
-        {
-            queue.Add(music);
-            TryToPlayMusic();
-            MessageQueue.Enqueue(MessageTopic.QueueUpdate);
-        }
-
-        public void Volume(double? target, bool mute = false)
-        {
-            Mute = mute;
-            if (Mute)
-            {
-                target = 0;
-            }
-            if (target is not null)
-            {
-                volume = (double)target;
-                volume = volume * volume * volume;
-                player.Volume = volume;
-            }
-            if (!MessageQueue.ToArray().Contains(MessageTopic.VolumeChange))
-                MessageQueue.Enqueue(MessageTopic.VolumeChange);
-        }
-
-        public string ChangeLoopMode()
-        {
-            LoopMode = LoopMode switch
-            {
-                LoopModeEnum.LoopNone => LoopModeEnum.LoopSingle,
-                LoopModeEnum.LoopSingle => LoopModeEnum.LoopAll,
-                LoopModeEnum.LoopAll => LoopModeEnum.LoopNone,
-                _ => LoopModeEnum.LoopNone,
-            };
-            return LoopMode switch
-            {
-                LoopModeEnum.LoopNone => "LoopNone",
-                LoopModeEnum.LoopSingle => "LoopSingle",
-                LoopModeEnum.LoopAll => "LoopAll",
-                _ => "ERROR",
-            };
-        }
-
-        public void Shuffle()
-        {
-            List<Music> temp;
-            if (nowPlaying is not null)
-                temp = queue.Except(new Music[] { nowPlaying }).ToList();
-            else
-                temp = queue.ToList();
-            queue.Clear();
-            foreach (var s in temp)
-            {
-                queue.Insert(Random.Shared.Next(queue.Count), s);
-            }
-            if (nowPlaying is not null)
-                queue.Insert(0, nowPlaying);
-            index = 1;
-            MessageQueue.Enqueue(MessageTopic.QueueUpdate);
-        }
-
-        public void Next()
-        {
-            if (nowPlaying is not null)
-            {
-                nowPlaying = null;
-                player.Stop();
-                index++;
-                TryToPlayMusic();
-            }
-        }
-
-        public void Back()
-        {
-            if (nowPlaying is not null)
-            {
-                index = index - 2 > 0 ? index - 2 : 0;
-                nowPlaying = null;
-                player.Stop();
-                TryToPlayMusic();
-            }
 
         }
-
         public void Pause()
         {
-            if (nowPlaying is not null)
-            {
-                if (status == PlayStatusEmun.Play)
-                {
-                    status = PlayStatusEmun.Pause;
-                    player.Pause();
-                }
-                else
-                {
-                    status = PlayStatusEmun.Play;
-                    player.Play();
-                }
-            }
-            else
-            {
-                index = 1;
-                TryToPlayMusic();
-            }
-            MessageQueue.Enqueue(MessageTopic.StatusUpdate);
-        }
 
-        public TimeSpan Position(TimeSpan? position = null)
-        {
-            if (position is not null)
-            {
-                player.Position = (TimeSpan)position;
-            }
-            return player.Position;
         }
-
-        public ObservableCollection<Music> GetQueue()
+        public void AddSong(string url)
         {
-            return queue;
+
+        }
+        public void AfterPlay()
+        {
+
+        }
+        public Song NowPlaying()
+        {
+            if (Status == PlayStatusEmun.Play)
+                return SongList[ListIndex];
+            return new Song();
         }
     }
 }
